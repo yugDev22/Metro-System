74523aeed28e5ec5254fb5d4268cf60398e989fe
dell <dell@DESKTOP-HLOR2G2>
2022-07-30 12:39:44 +0530

    Commit by V.

--- Metro-Project/src/main/java/com/metro/persistence/StationDao.java
+++ Metro-Project/src/main/java/com/metro/persistence/StationDao.java
@@ -1,9 +1,12 @@
 package com.metro.persistence;
 
+import java.util.List;
+
 import com.metro.bean.Station;
 
 public interface StationDao {
 	
 	public Station searchStationById(int stationId);
 	public int addStation(Station station);
+	public List<Station> getAllStations();
 }
--- Metro-Project/src/main/java/com/metro/persistence/StationDaoImpl.java
+++ Metro-Project/src/main/java/com/metro/persistence/StationDaoImpl.java
@@ -1,19 +1,100 @@
 package com.metro.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.metro.bean.Station;
+
 
 public class StationDaoImpl implements StationDao {
 
 	@Override
 	public Station searchStationById(int stationId) {
-		// TODO Auto-generated method stub
-		return null;
+		Station station = null;
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/wileyProject", "root", "root");
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("SELECT * FROM stations where stationId=?");) {
+
+			preparedStatement.setInt(1, stationId);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				int id = resultSet.getInt("stationId");
+				String name = resultSet.getString("stationName");
+				int prevStationId = resultSet.getInt("previousStationId");
+				int nextStationId = resultSet.getInt("nextStationId");
+
+
+				station = new Station(id, name, prevStationId, nextStationId);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return station;
 	}
 
 	@Override
 	public int addStation(Station station) {
-		// TODO Auto-generated method stub
-		return 0;
+		int rows = 0;
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/wileyProject", "root", "root");
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO stations values(?,?,?,?)");
+
+			preparedStatement.setInt(1, station.getStationId());
+			preparedStatement.setString(2, station.getStationName());
+			preparedStatement.setInt(3, station.getPrevStationId());
+			preparedStatement.setInt(4, station.getNextStationId());
+			
+			if(station.getPrevStationId()!=-1)
+			{
+				PreparedStatement preparedStatement2 = connection.prepareStatement("UPDATE stations set nextStationId=? where stationId=?");
+				preparedStatement2.setInt(1, station.getStationId());
+				preparedStatement2.setInt(2, station.getPrevStationId());
+				preparedStatement2.executeUpdate();
+			}
+			
+			rows = preparedStatement.executeUpdate();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return rows;
+	}
+
+	@Override
+	public List<Station> getAllStations() 
+	{
+		List<Station> stationList = new ArrayList<Station>();
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/wileyProject", "root", "root"); Statement statement = connection.createStatement();) {
+
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM stations");
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt("stationId");
+				String name = resultSet.getString("stationName");
+				int prevStationId = resultSet.getInt("previousStationId");
+				int nextStationId = resultSet.getInt("nextStationId");
+
+				stationList.add(new Station(id, name, prevStationId, nextStationId));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return stationList;
 	}
 
 }
